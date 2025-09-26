9bd97617ae10cf48d3bae4575920cb782af6212e
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-09-26 22:54:54 +0800

    birthdate calendar

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,8 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.Toast
+import android.app.DatePickerDialog
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,32 +23,46 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val fullname = findViewById<EditText>(R.id.edit_fullname)
-        val username = findViewById<EditText>(R.id.editTextText4)
+        val username = findViewById<EditText>(R.id.edit_user)
+        val birthday = findViewById<EditText>(R.id.edit_birthdate)
         val uemail = findViewById<EditText>(R.id.edit_email)
         val password = findViewById<EditText>(R.id.edit_pass)
         val conf_pass = findViewById<EditText>(R.id.confirm_pass)
         val signbtn = findViewById<Button>(R.id.signbtn)
 
+        val calendar = Calendar.getInstance()
+
+        birthday.setOnClickListener {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    val birthdate = "${selectedMonth + 1}-$selectedDay-$selectedYear"
+                    birthday.setText(birthdate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+
         signbtn.setOnClickListener{
             val name = fullname.text.toString().trim()
             val user = username.text.toString().trim()
+            val bday = birthday.text.toString().trim()
             val email = uemail.text.toString().trim()
             val pass = password.text.toString()
             val c_pass = conf_pass.text.toString()
 
-            if (name.isEmpty()) {
-                Toast.makeText(this, "Fullname is required", Toast.LENGTH_LONG).show()
-            } else if (user.isEmpty()) {
-                Toast.makeText(this, "Username is required", Toast.LENGTH_LONG).show()
-            } else if (email.isEmpty() || pass.isEmpty() || c_pass.isEmpty()) {
+            if (name.isEmpty() || user.isEmpty() || bday.isEmpty() || email.isEmpty() || pass.isEmpty() || c_pass.isEmpty()) {
                 Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show()
             } else if (pass != c_pass) {
-                Toast.makeText(this, "Password does not match", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(this, "Sign up successful", Toast.LENGTH_LONG).show()
                 Toast.makeText(
                     this,
-                    "Full Name: $name\nUsername: $user\nEmail: $email\nPassword: $pass",
+                    "Full Name: $name\nUsername: $user\nBirthdate: $bday\nEmail: $email\nPassword: $pass",
                     Toast.LENGTH_LONG
                 ).show()
             }
