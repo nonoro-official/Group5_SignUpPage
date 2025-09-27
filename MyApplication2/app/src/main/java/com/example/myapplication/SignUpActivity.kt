e6464b55166bdfac098369849631178967dab00e
Noah Peñaranda <noah.c.penaranda@gmail.com>
2025-09-27 19:28:23 +0800

    refactored and added signup + login + gallery activities

--- MyApplication2/app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -16,12 +16,12 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
